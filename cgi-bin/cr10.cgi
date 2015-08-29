640a2803fcfe2c0a1692bcfeb4b61343577d3e16
kakl <kakl@astro.cz>
2015-08-29 13:21:52 +0200

    server tested version

---
+++ cgi-bin/cr10.cgi
@@ -0,0 +1,1 @@
+/home/aircraft/public_html/CR10/cgi-bin/cr10.py
--- cgi-bin/cr10.py
+++ cgi-bin/cr10.py
@@ -9,6 +9,9 @@ import sys
 sys.stderr = sys.stdout
 #'''
 
+activate_this = '/home/aircraft/aircraft/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
+
 # Import modules for CGI handling 
 import cgi, cgitb 
 
@@ -30,16 +33,16 @@ days = form.getvalue('days')
 
 days_view = int(days)
 
-df3 = pd.read_csv('./data/LSradiation.csv', sep=',', header=0, parse_dates='time')  # load FD data
+df3 = pd.read_csv('~aircraft/public_html/CR10/data/LSradiation.csv', sep=',', header=0, parse_dates='time')  # load FD data
 df3['time'] = pd.to_datetime(df3['time'])
 df3 = df3.set_index('time')
 df3 = df3.sort_index()
 
-df2 = pd.read_csv('./data/radiation_avg.csv', sep=',', header=0, parse_dates='time' ) # load Liulin data
+df2 = pd.read_csv('~aircraft/public_html/CR10/data/radiation_avg.csv', sep=',', header=0, parse_dates='time' ) # load Liulin data
 df2['date'] = pd.to_datetime(df2['date'])
 df2 = df2.set_index('date')
-df2['H'] = df2['H'].astype(float)
-df2['HH'] = df2['HH'].astype(float) * 3 + 20
+df2['DSi'] = df2['DSi'].astype(float)
+df2['DSi_corr'] = df2['DSi_corr'].astype(float) * 3 + 20
 
 df2['lat'] = df2['lat'] / 20 - 10
 df2['alt'] = df2['alt'] / 3048 - 20 # feets to meters plus some shift
@@ -57,7 +60,7 @@ plt.title(FD)
 plt.axvline(x=pd.to_datetime(FD), color='grey', ls='--') # plot dashed vertical line at the FD time
 
 
-plt.savefig('./ble.png')
+plt.savefig('/home/aircraft/public_html/CR10/data/ble.png')
 
 # HTML code
 print 'Content-type:text/html\r\n\r\n'
@@ -65,17 +68,18 @@ print '<html>'
 print '<head>'
 print '<title>CR10</title>'
 print '<link rel="stylesheet" type="text/css" href="../style/aircraft.css">'
+print '<link rel='shortcut icon' type='image/x-icon' href='../style/favicon.ico' />'
 print '</head>'
 print '<body>'
 
 print "<h1>CR10 database</h1>"
-print '<form action="/cgi-bin/cr10.py" method="get">'
+print '<form action="~aircraft/bin/cr10.py" method="get">'
 print 'Day: <input type="text" name="FD" value=%s>  &nbsp; &nbsp; Window +-: <input type="text" name="days" value=%s /> days &nbsp; &nbsp; ' % (FD, days)
 print '<input type="submit" value="Submit" />'
 print '</form>'
 
-print '<img src="../ble.png">'
-print '<p>When using these data, please read this <a href="../licence.html">info</a>.</p>'
+print '<img src="../data/ble.png">'
+print '<p>When using these data, please read this <a href="licence.html">info</a>.</p>'
 
 print '</body>'
 print '</html>'
