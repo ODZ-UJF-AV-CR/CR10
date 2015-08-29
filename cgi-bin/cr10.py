8509309dc1fd437837260a96cd28f8eb7a4eeab1
kakl <kakl@astro.cz>
2015-08-29 15:39:05 +0200

    design changes

--- cgi-bin/cr10.py
+++ cgi-bin/cr10.py
@@ -51,16 +51,21 @@ datetime.timedelta(days=1)
 from_time = str(pd.to_datetime(FD) + datetime.timedelta(days=-days_view))
 to_time = str(pd.to_datetime(FD) + datetime.timedelta(days=days_view))
 
-df2[from_time: to_time].plot(figsize=(12,8),lw='1') 
-#df2[from_time: to_time]['lat'].plot(figsize=(12,8),lw='1', label='latitude') 
+
+df2[from_time: to_time].plot(figsize=(12,8),fontsize=10) 
+#df2[from_time: to_time]['lat'].plot(figsize=(12,9),lw='1', label='latitude') 
 df3[from_time: to_time]['FD'] = df3[from_time: to_time]['FD'].sub(90) # normalize FD data
-df3[from_time: to_time]['FD'].plot(figsize=(12,8),lw='1', label='NM LS') 
-plt.title(FD)  
+df3[from_time: to_time]['FD'].plot(lw='1',  ls=':', label='NM LS')
+plt.ylabel('[arbitrary unit]',fontsize=10) # Y axis label
+plt.xlabel('UT') # X axis label
+plt.title(FD)  # print central time
 
 plt.axvline(x=pd.to_datetime(FD), color='grey', ls='--') # plot dashed vertical line at the FD time
 
+plt.legend(fontsize=10)
 
-plt.savefig('/home/aircraft/public_html/CR10/data/ble.png')
+plt.tight_layout()  # reduce margins
+plt.savefig('/home/aircraft/public_html/CR10/data/ble.png')  # save plot to file
 
 # HTML code
 print 'Content-type:text/html\r\n\r\n'
@@ -68,18 +73,18 @@ print '<html>'
 print '<head>'
 print '<title>CR10</title>'
 print '<link rel="stylesheet" type="text/css" href="../style/aircraft.css">'
-print '<link rel='shortcut icon' type='image/x-icon' href='../style/favicon.ico' />'
+print '<link rel="shortcut icon" type="image/x-icon" href="../style/favicon.ico" />'
 print '</head>'
 print '<body>'
 
 print "<h1>CR10 database</h1>"
-print '<form action="~aircraft/bin/cr10.py" method="get">'
+print '<form action="cr10.cgi" method="get">'
 print 'Day: <input type="text" name="FD" value=%s>  &nbsp; &nbsp; Window +-: <input type="text" name="days" value=%s /> days &nbsp; &nbsp; ' % (FD, days)
 print '<input type="submit" value="Submit" />'
 print '</form>'
 
 print '<img src="../data/ble.png">'
-print '<p>When using these data, please read this <a href="licence.html">info</a>.</p>'
+print '<p>When using these data, please read this <a href="../license.html">info</a>.</p>'
 
 print '</body>'
 print '</html>'
